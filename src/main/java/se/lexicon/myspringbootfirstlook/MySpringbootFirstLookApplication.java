89c4de18c9cb1b5eba6762f739757875e6eb8dfc
Rubotnic <info@rubotnic.com>
2022-09-07 14:03:57 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/myspringbootfirstlook/MySpringbootFirstLookApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.myspringbootfirstlook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootFirstLookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringbootFirstLookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/myspringbootfirstlook/MySpringbootFirstLookApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.myspringbootfirstlook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootFirstLookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
